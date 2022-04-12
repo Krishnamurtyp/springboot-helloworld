6e898f1a682b71948d05bcb05e8471ce4e9a64f7
Krishnamurtyp <krishnamurthy.pradhan@gmail.com>
2022-04-12 11:27:48 +0530

    Update HomeController.java
    added a message in rest controller

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from GazGeek!";
+        return "Hello from Krishna to our NPCI team...!";
     }
 
 }
